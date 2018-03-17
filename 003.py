5984660317b9779014465381eb405bb7aa1581c2
Isa Ishangulyyev <isaishan424@gmail.com>
2018-03-17 23:41:45 +0300

    Added 003.py

---
+++ 003.py
@@ -0,0 +1,4 @@
+# The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143 ?
+import utils
+
+print(max(utils.get_prime_factors_list(600851475143)))
--- utils.py
+++ utils.py
@@ -4,3 +4,20 @@ def is_even(n):
 
 def is_odd(n):
 	return n % 2 > 0
+
+
+def get_prime_factors_list(n):
+	if n < 3:
+		return [n]
+
+	prime_factors = []
+	i = 2
+
+	while i <= n:
+		while n % i == 0:
+			prime_factors.append(i)
+			n = n / i
+
+		i += 1
+
+	return prime_factors
